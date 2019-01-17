a608d6b6d8a3898207c112491da36ba81cf5f74d
Marek Kotowski <marek.kotowski87@gmail.com>
2019-01-17 22:47:08 +0100

    Add total calories

--- app.js
+++ app.js
@@ -43,6 +43,20 @@ const ItemCtrl = (function() {
     },
     logData: function() {
       return data;
+    },
+    getTotalCalories: function() {
+      let total = 0;
+
+      // Loop through items add cals
+      data.items.forEach(function(item) {
+        total += item.calories;
+      });
+
+      // Set total cal in data structure
+      data.totalCalories = total;
+
+      // Return total
+      return data.totalCalories;
     }
   };
 })();
@@ -53,7 +67,8 @@ const UICtrl = (function() {
     itemList: '#item-list',
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
-    itemCaloriesInput: '#item-calories'
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories'
   };
 
   // Public controller
@@ -109,6 +124,11 @@ const UICtrl = (function() {
     },
     hideList: function() {
       document.querySelector(UISelectors.itemList).style.display = 'none';
+    },
+    showTotalCalories: function(totalCalories) {
+      document.querySelector(
+        UISelectors.totalCalories
+      ).textContent = totalCalories;
     }
   };
 })();
@@ -139,6 +159,11 @@ const App = (function(ItemCtrl, UICtrl) {
       // Add item to UI List
       UICtrl.addListItem(newItem);
 
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
       // Clear fields
       UICtrl.clearInput();
     }
@@ -159,6 +184,11 @@ const App = (function(ItemCtrl, UICtrl) {
         // Populate list with items
         UICtrl.populateItemList(items);
       }
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
 
       // Load Event Listeners
       loadEventListeners();
