411bdd8b8e4aaae0aeec3c8ebb76f1207bbdd491
Marek Kotowski <marek.kotowski87@gmail.com>
2019-01-17 19:12:34 +0100

    Add Item to UI

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const ItemCtrl = (function() {
   // Data structure / state
   const data = {
     items: [
-      { id: 0, name: 'Steak Dinner', calories: 1200 },
-      { id: 1, name: 'Cookie', calories: 400 },
-      { id: 2, name: 'Eggs', calories: 300 }
+      // { id: 0, name: 'Steak Dinner', calories: 1200 },
+      // { id: 1, name: 'Cookie', calories: 400 },
+      // { id: 2, name: 'Eggs', calories: 300 }
     ],
     currentItem: null,
     totalCalories: 0
@@ -27,23 +27,18 @@ const ItemCtrl = (function() {
     },
     addItem: function(name, calories) {
       let ID;
-
-      // Create ID
+      // // Create ID
       if (data.items.length > 0) {
-        ID = data.items[data.length - 1].id + 1;
+        //ID = data.items[data.items.length - 1].id + 1;
       } else {
         ID = 0;
       }
-
-      // Calories to number
+      // // Calories to number
       calories = parseInt(calories);
-
-      // Create new item
+      // // Create new item
       newItem = new Item(ID, name, calories);
-
-      // Add to items array
+      // // Add to items array
       data.items.push(newItem);
-
       return newItem;
     },
     logData: function() {
@@ -87,6 +82,33 @@ const UICtrl = (function() {
     },
     getSelectors: function() {
       return UISelectors;
+    },
+    addListItem: function(item) {
+      // Show the list
+      document.querySelector(UISelectors.itemList).style.display = 'block';
+      // Create lo element
+      const li = document.createElement('li');
+      // Add class
+      li.className = 'collection-item';
+      // Add id
+      li.id = `item-${item.id}`;
+      li.innerHTML = `<strong>${item.name}: </strong> <em>${
+        item.calories
+      } Calories</em>
+      <a href="#" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+      </a>`;
+      // Insert item
+      document
+        .querySelector(UISelectors.itemList)
+        .insertAdjacentElement('beforeend', li);
+    },
+    clearInput: function() {
+      document.querySelector(UISelectors.itemNameInput).value = '';
+      document.querySelector(UISelectors.itemCaloriesInput).value = '';
+    },
+    hideList: function() {
+      document.querySelector(UISelectors.itemList).style.display = 'none';
     }
   };
 })();
@@ -109,10 +131,16 @@ const App = (function(ItemCtrl, UICtrl) {
     // Get form input from UI Controller
     const input = UICtrl.getItemInput();
 
-    // Check for name and calories input
+    //Check for name and calories input
     if (input.name !== '' && input.calories !== '') {
       // Add item
       const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+      // Add item to UI List
+      UICtrl.addListItem(newItem);
+
+      // Clear fields
+      UICtrl.clearInput();
     }
 
     e.preventDefault();
@@ -124,8 +152,13 @@ const App = (function(ItemCtrl, UICtrl) {
       // Fetch items from data structure
       const items = ItemCtrl.getItems();
 
-      // Populate list with items
-      UICtrl.populateItemList(items);
+      // Check if any items
+      if (items.length === 0) {
+        UICtrl.hideList();
+      } else {
+        // Populate list with items
+        UICtrl.populateItemList(items);
+      }
 
       // Load Event Listeners
       loadEventListeners();
